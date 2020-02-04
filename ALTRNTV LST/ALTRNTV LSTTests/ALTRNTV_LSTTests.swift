3224f01d75cc5fe8b645a9008f016268dd70feab
jesseleeruiz <jesseleeruiz@gmail.com>
2020-02-03 19:57:01 -1000

    Added XCode Project with Core Data, Unit Tests, UI Tests and CloudKit.

---
+++ ALTRNTV LST/ALTRNTV LSTTests/ALTRNTV_LSTTests.swift
@@ -0,0 +1,34 @@
+//
+//  ALTRNTV_LSTTests.swift
+//  ALTRNTV LSTTests
+//
+//  Created by Jesse Ruiz on 2/3/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import XCTest
+@testable import ALTRNTV_LST
+
+class ALTRNTV_LSTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
