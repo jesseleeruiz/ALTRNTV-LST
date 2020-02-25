9b7ca088bd5347c8ca94559ac681247b32535999
jesseleeruiz <jesseleeruiz@gmail.com>
2020-02-25 11:40:53 -1000

    Added TaskRepresentation file.

---
+++ ALTRNTV LST/ALTRNTV LST/Models/TaskRepresentation.swift
@@ -0,0 +1,14 @@
+//
+//  TaskRepresentation.swift
+//  ALTRNTV LST
+//
+//  Created by Jesse Ruiz on 2/25/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import Foundation
+
+struct TaskRepresentation: Codable {
+    var task: String?
+    var timestamp: Date?
+}
