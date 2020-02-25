f03967f83c643045d287edd972a38b690737cd74
jesseleeruiz <jesseleeruiz@gmail.com>
2020-02-25 11:38:41 -1000

    Added Core Data Stack file.

--- ALTRNTV LST/ALTRNTV LST/View Controllers/TasksTableViewController.swift
+++ ALTRNTV LST/ALTRNTV LST/View Controllers/TasksTableViewController.swift
@@ -40,16 +40,12 @@ class TasksTableViewController: UITableViewController {
         return cell
     }
     
-
-    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    
 
-    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -60,14 +56,10 @@ class TasksTableViewController: UITableViewController {
         }    
     }
     
-
-    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
-    
-
     
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
@@ -85,5 +77,4 @@ class TasksTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
